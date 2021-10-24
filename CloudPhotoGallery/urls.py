c85f6cd8e356435c504b2ceb20d252b8a91c4500
eugenia-cell <63282316+eugenia-cell@users.noreply.github.com>
2021-10-24 10:00:19 +0800

    Update urls.py

--- CloudPhotoGallery/urls.py
+++ CloudPhotoGallery/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
